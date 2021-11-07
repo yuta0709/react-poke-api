e284dc2b0b8e64f0f75918613c20694c2de9437f
yuta0709 <yuta0709dev@gmail.com>
2021-11-08 01:13:47 +0900

    Add type definitions for poke api

---
+++ src/common/api/types/pokedex.ts
@@ -0,0 +1,12 @@
+export interface Pokedex {
+    name: string,
+    url: string
+};
+
+export interface PokedexList{
+    count: number,
+    next?: string,
+    previous?: string,
+    results: Pokedex[]
+
+};
