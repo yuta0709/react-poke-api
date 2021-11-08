87e8a9dcbec909b729a75025c39526ce4c69ad22
yuta0709 <yuta0709dev@gmail.com>
2021-11-08 11:27:22 +0900

    Fix: format

--- src/common/api/client.ts
+++ src/common/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { baseURL } from "../constants/apiPaths";
 
 export default axios.create({
-    baseURL: baseURL,
-    headers: {"Content-Type": "application/json"},
-    responseType: "json"
+  baseURL: baseURL,
+  headers: { "Content-Type": "application/json" },
+  responseType: "json",
 });
--- src/common/api/types/pokedex.ts
+++ src/common/api/types/pokedex.ts
@@ -1,12 +1,11 @@
 export interface Pokedex {
-    name: string,
-    url: string
-};
+  name: string;
+  url: string;
+}
 
-export interface PokedexList{
-    count: number,
-    next?: string,
-    previous?: string,
-    results: Pokedex[]
-
-};
+export interface PokedexList {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: Pokedex[];
+}
